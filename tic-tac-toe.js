eb02b626bee50e558a3e5cac97749ad3623459f7
Quan-R <55651890+Quan-R@users.noreply.github.com>
2019-10-21 23:10:06 -0500

    Changes to Board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,39 @@
 
-window.onload=function(){
-        this.loadBoard()
-    
-    }
+var score = {'X': 0,    'O': 0};
+var moves = 0;
+var turn = [];
+document.addEventListener('DOMContentLoaded', loadBoard);
+
+   
     function loadBoard(){
-        var board = document.querySelectorAll('#board div');
-        for(i=0; i<board.length; i++){
-            board[i].className = "square";
+        var board = document.getElementById("board");
+        var squares = board.children; 
+        for(let i=0; i<squares.length; i++){
+            squares[i].classList.add("square");
+        }
+        for(let i=0; i<squares.length; i++){
+            squares[i].onmouseover = function(){squares[i].classList.toggle("hover",true)}
+            squares[i].onmouseout = function(){squares[i].classList.toggle("hover",false)}
+        }
+        for(let i=0; i<squares.length; i++){
+            if(turn.length==0){
+                squares[i].classList.add("X");
+                squares[i].innerHTML = ("X");
+                gameState.push("X");
+            }
+            else{
+                if(turn[turn.length-1]==-'X'){
+                    squares[i].classList.add("O");
+                    squares[i].innerHTML=("O");
+                    turn.push("O");
+                }
+                else{
+                    squares[i].classList.add("O");
+                    squares[i].innerHTML=("X");
+                    turn.push("X");
+                }
+            }
         }
     }
+
     
